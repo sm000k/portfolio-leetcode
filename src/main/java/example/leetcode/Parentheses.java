8e6c992294476a087ab35c9f50697835e39995a2
sm000k <pw.lacki@gmail.com>
2024-02-03 21:28:23 +0100

    -change to ParenthesesMap.containsvalue(symbo) instead if symbol is {, (, [, etc; -added else if to link cases -Initialization the parenthesesMap variable outside the solution method. -added another test : "{[()(){}]{}}"

--- src/main/java/example/leetcode/Parentheses.java
+++ src/main/java/example/leetcode/Parentheses.java
@@ -1,28 +1,32 @@
 package example.leetcode;
 
-import java.util.Arrays;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
 public class Parentheses {
-    public static boolean isValid(String s) {
 
-        Map<Character, Character> parenthesesMap = new HashMap<>();
+    private static final Map<Character, Character> parenthesesMap = new HashMap<>();
+
+    static {
         parenthesesMap.put('}', '{');
         parenthesesMap.put(']', '[');
         parenthesesMap.put(')', '(');
+    }
+
+    public static boolean isValid(String s) {
+
+
         if (s.isEmpty() || s.length() % 2 != 0) return false;
 
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             Character symbol = s.charAt(i);
 
-            if (Arrays.asList('{', '(', '[').contains(symbol)) {
+            if (parenthesesMap.containsValue(symbol)) {
                 stack.push(symbol);
-            }
-
-            if (parenthesesMap.containsKey(symbol)) {
+            } else if (parenthesesMap.containsKey(symbol)) {
                 if (stack.isEmpty()) return false;
                 if (stack.peek() == parenthesesMap.get(symbol)) {
                     stack.pop();
@@ -36,7 +40,7 @@ public class Parentheses {
 
     public static void main(String[] args) {
 
-        String[] inputs = {"()", "()[]{}", "(]", "]", "){","{[()]}"};
+        String[] inputs = {"()", "()[]{}", "(]", "]", "){", "{[()]}", "{[()(){}]{}}"};
         for (String input : inputs) {
             System.out.println(input + " : " + isValid(input));
         }
