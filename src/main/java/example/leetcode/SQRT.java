3cbe0c7aee17ad5cb359acc05f66c15172d6a1d9
sm000k <pw.lacki@gmail.com>
2024-02-07 18:35:03 +0100

    8. - binary search solution

--- src/main/java/example/leetcode/SQRT.java
+++ src/main/java/example/leetcode/SQRT.java
@@ -1,20 +1,38 @@
 package example.leetcode;
 
 public class SQRT {
+    public static void unitTestSQRT(Integer input, Integer expected) {
+        Integer output = mySqrt(input);
+        System.out.println();
+        System.out.print(output.equals(expected));
+    }
+
     public static int mySqrt(int x) {
-        double testValue = 45000;
-        double testVar = 0;
-        while (testVar <= x) {
 
-            testValue++;
-            testVar = testValue * testValue;
+        if (x == 0) {
+            return 0;
         }
+        int left = 1;
+        int right = Integer.MAX_VALUE;
+        int mid = 0;
+        while (true) {
+            mid = left + (right - left) / 2;
+            if (mid > x / mid) {
+                right = mid - 1;
+            } else {
+                if ((mid + 1) > x / (mid + 1)) {
+                    return mid;
+                }
+                left = mid + 1;
+            }
 
-        return (int) testValue - 1;
+
+        }
     }
 
     public static void main(String[] args) {
-//        System.out.println(mySqrt(155));
-        System.out.println(mySqrt(2147395600));
+
+        unitTestSQRT(4, 2);
+        unitTestSQRT(8, 2);
     }
 }
