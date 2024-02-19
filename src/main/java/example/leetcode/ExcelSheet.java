82c69b5054fe071b6fbe1f988678e378070e1a6b
sm000k <smok.lacki@gmail.com>
2024-02-19 17:39:38 +0100

    16. Excel sheet - first working try

---
+++ src/main/java/example/leetcode/ExcelSheet.java
@@ -0,0 +1,34 @@
+package example.leetcode;
+
+
+
+
+//https://leetcode.com/problems/excel-sheet-column-number/description/
+public class ExcelSheet {
+
+
+    public static int titleToNumber(String columnTitle) {
+        int letterPosition = columnTitle.length();
+        int columnNumber = 0;
+        for (int Index = 0; Index < columnTitle.length(); Index++) {
+            int Letter = columnTitle.charAt(Index) - 64;
+            columnNumber += (int)Math.pow(26, letterPosition - 1) * Letter;
+            letterPosition--;
+        }
+
+        return columnNumber;
+    }
+
+    static void unitTestTitleToNumber(String input, int expected) {
+        System.out.print("input = " + input);
+        System.out.println(" ");
+        System.out.print("expected = " + expected);
+        System.out.println(" = " + (titleToNumber(input) == expected));
+    }
+
+    public static void main(String [] args) {
+        unitTestTitleToNumber("AA", 27);
+        unitTestTitleToNumber("AAA", 703);
+        unitTestTitleToNumber("ZY", 701);
+    }
+}
