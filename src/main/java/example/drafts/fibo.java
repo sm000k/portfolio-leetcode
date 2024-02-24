1cda2b4fc4f5c899217f35f99e997ea70411bd8d
sm000k <smok.lacki@gmail.com>
2024-02-24 14:36:12 +0100

    fibo

---
+++ src/main/java/example/drafts/fibo.java
@@ -0,0 +1,20 @@
+package example.drafts;
+
+public class fibo {
+    public static int fibonacci(int n) {
+
+        System.out.println(n);
+        if (n<2) {
+            return n;
+        }
+        else
+        {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int fibonacci = fibonacci(3);
+        System.out.println("fibonacci = " + fibonacci);
+    }
+}
